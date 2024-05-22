a62b6addcdbab667599d5ad96d4d0c1c84ab990d
Bogdan <bogdan.dobai@gmail.com>
2024-05-22 15:04:56 +0200

    add widgets

---
+++ ios/HelloWidgets/HelloWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  HelloWidgetsBundle.swift
+//  HelloWidgets
+//
+//  Created by Bogdan Dobai on 22.05.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HelloWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        HelloWidgets()
+    }
+}
